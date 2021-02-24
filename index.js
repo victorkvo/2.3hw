1f615cd4ea8334cda6765dd4eae28acd4f3216e8
Victor Vo <79146455+victorkvo@users.noreply.github.com>
2021-02-24 12:20:35 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let cups = prompt("How many cups of coffee have you had?")
+let coffee = Number(cups)
+
+if (coffee < 3) {
+    console.log("Yes I'll take another cup of coffee.");
+} else 
+{
+    console.log("I think I'm okay for now.");
+} 
+
+
+
+let miles = prompt("How many miles has it been since your last oil change?")
+let mileage = Number(miles)
+
+if (mileage > 500) {
+    console.log("Yes I think I need an oil change.");
+} else {
+    console.log("My car doesn't need an oil change yet.");
+} 
