6d502cde90e3543edbbd0298364571586d197d4f
Victor Vo <79146455+victorkvo@users.noreply.github.com>
2021-02-24 13:54:15 -0600

    includes second hw

---
+++ index2.js
@@ -0,0 +1,7 @@
+var myArray = ["What is the meaning of life?", "The meaning of life is", 42];
+
+// TODO: modify this code
+for (var i = 0; i < myArray.length; i++)
+{
+     console.log(myArray[i]);
+}
